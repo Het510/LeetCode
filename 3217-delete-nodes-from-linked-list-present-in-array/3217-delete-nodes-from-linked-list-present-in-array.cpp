399449805d7dbfee06d531aa6fc9110dc8f30a00
het rathod <hetrathod49@gmail.com>
2026-08-11 14:07:17 +0530

    Time: 107 ms (32.49%), Space: 263.2 MB (76.62%) - LeetHub

---
+++ 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    ListNode* modifiedList(std::vector<int>& nums, ListNode* head) {
+        unordered_set<int> numSet(nums.begin(), nums.end());
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* curr = dummy;
+        while (curr->next) 
+        {
+            if (numSet.count(curr->next->val)) 
+            {
+                curr->next = curr->next->next;
+            } 
+            else 
+            {
+                curr = curr->next;
+            }
+        }
+        return dummy->next;
+    }
+};
